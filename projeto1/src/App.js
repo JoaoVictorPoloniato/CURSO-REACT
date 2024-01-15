291ec8bdac37369615dfc624ae5fab96d3f5b129
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-01-15 15:07:17 -0400

    Update App.js
    a

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -1,51 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.handlePClick = this.handlePClick.bind(this);
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O primeiro',
+        body: 'O primeiro corpo'
+      },
+       {
+        id: 2,
+        title: 'O segundo',
+        body: 'O segundo corpo'
+       },
+       {
+        id: 3,
+        title: 'O terceiro',
+        body: 'O terceiro corpo'
+       },
+    ]
+  };
+  
+  componentDidMount () {
+    console.log('OI');
+  }
 
-    this.state = {
-      name: 'Clique aqui para saber o nome do responsavel do projeto'
-    };
-}
-
-
-handlePClick() {
-  this.setState({ name: 'João Victor Poloniato Buss'});
-}
-
-handleAClick = (event) => {
-  event.preventDefault();
-  window.open("https://github.com/JoaoVictorPoloniato")
-  const { counter } = this.state;
-  this.setState({ counter: counter + 1 });
-}
 
   render(){
-    const { name, counter } = this.state;
+    const { posts } = this.state;
+
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handlePClick}>
-          {name} { counter }
-        </p>
-        <a
-          onClick={ this.handleAClick }
-          className="App-link"
-          href="https://github.com/JoaoVictorPoloniato"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub do criador do site
-       </a>
-      </header>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
-  );
-}
-}
+    );
+  }
+};
+
 
 export default App;
