d0442998b7aed3d1c49c682d8fd0d3adae705aee
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-01-15 14:16:16 -0400

    Update
    Install the project in my computer

---
+++ projeto1/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this);
+
+    this.state = {
+      name: 'Clique aqui para saber o nome do responsavel do projeto'
+    };
+}
+
+
+handlePClick() {
+  this.setState({ name: 'João Victor Poloniato Buss'});
+}
+
+handleAClick = (event) => {
+  event.preventDefault();
+  window.open("https://github.com/JoaoVictorPoloniato")
+  const { counter } = this.state;
+  this.setState({ counter: counter + 1 });
+}
+
+  render(){
+    const { name, counter } = this.state;
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={this.handlePClick}>
+          {name} { counter }
+        </p>
+        <a
+          onClick={ this.handleAClick }
+          className="App-link"
+          href="https://github.com/JoaoVictorPoloniato"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub do criador do site nao deu teste aparentemente sss
+       </a>
+      </header>
+    </div>
+  );
+}
+}
+
+export default App;
