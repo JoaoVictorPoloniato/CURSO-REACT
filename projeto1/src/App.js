40da7e633673d85ceb17caf20d6dfb38c0858899
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-01-17 14:12:16 -0400

    Arrumação da grade

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -11,14 +11,23 @@ class App extends Component {
   }
 
   loadPosts = async () => {
-    try {
       const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const postsJson = await postsResponse.json();
-      this.setState({ posts: postsJson });
-    } catch (error) {
-      console.error('Erro ao carregar posts:', error);
-    }
-  };
+
+      const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+      const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+      const postsJson = await posts.json();
+
+      const photosJson = await photos.json();
+
+      const postsAndPhotos = postsJson.map((post, index) => {
+        return { ...post, cover: photosJson[index]. url}
+      });
+
+
+      this.setState({posts: postsAndPhotos });
+  }
 
   render() {
     const { posts } = this.state;
@@ -28,12 +37,15 @@ class App extends Component {
         <div className="posts">
           {posts.map(post => (
             <div key={post.id} className="post">
+              <img src={post.cover} alt={post.title} />
               <div className="post-content">
                 <h1>{post.title}</h1>
                 <p>{post.body}</p>
               </div>
             </div>
-          ))}
+          )
+        )
+      }
         </div>
       </section>
     );
