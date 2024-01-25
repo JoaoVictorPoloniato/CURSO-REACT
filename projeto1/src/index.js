ff2a51d1995d30c4a3ce34b4b130aa92e776b8f6
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2024-01-25 15:59:33 -0400

    My update

--- projeto1/src/index.js
+++ projeto1/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Home } from './templates/Home/index' 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./templates/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Home />
--- projeto1/src/templates/Home/index.jsx
+++ projeto1/src/templates/Home/index.jsx
@@ -1,34 +1,33 @@
+import React, { Component } from "react";
 
+import "./styles.css";
 
-import React, { Component } from 'react';
-
-import './styles.css';
-
-import { Posts } from '../../components/Posts';
-import {loadPosts} from '../../utils/load-posts';
+import { Posts } from "../../components/Posts";
+import { loadPosts } from "../../utils/load-posts";
 
 class Home extends Component {
   state = {
     posts: [],
   };
 
- async componentDidMount() {
+  async componentDidMount() {
     await this.loadPosts();
   }
 
-  loadPosts = async () => { 
-    const postsAndPhotos = await loadPosts 
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts;
     this.setState({ posts: postsAndPhotos });
-  }
-  
+  };
+
   render() {
     const { posts } = this.state;
 
-    console.log('Posts:', posts);
+    console.log("Posts:", posts);
 
     return (
       <section className="container">
-        <Posts posts={posts} />
+        {/* <Posts posts={posts} /> */}
+        <h1>Home</h1>
       </section>
     );
   }
