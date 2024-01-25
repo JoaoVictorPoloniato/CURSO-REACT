e2ad6bfdbf166c482e070f32733675ba3f93f4da
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-01-25 14:24:11 -0400

    Update jsx

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -1,30 +1,22 @@
 // App.js
 import React, { Component } from 'react';
+
 import './App.css';
-import { PostCard } from './components/PostCard';
+
+import { Posts } from './components/Posts';
+import {loadPosts} from './utils/load-posts';
 
 class App extends Component {
   state = {
     posts: [],
   };
 
-  componentDidMount() {
-    this.loadPosts();
+ async componentDidMount() {
+    await this.loadPosts();
   }
 
-  loadPosts = async () => {
-    const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = await fetch('https://jsonplaceholder.typicode.com/photos');
-    
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-  
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-  
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url };
-    });
-    
+  loadPosts = async () => { 
+    const postsAndPhotos = await loadPosts 
     this.setState({ posts: postsAndPhotos });
   }
   
@@ -35,17 +27,7 @@ class App extends Component {
 
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map((post) => (
-            <PostCard
-                key={post.id}
-                title={post.title}
-                body={post.title}
-                cover={post.cover}
-                id={post.id}
-                />
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
