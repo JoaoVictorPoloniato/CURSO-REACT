29fb0af52dcb220f20601918cf84c5b52bfcf273
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-01-25 16:25:34 -0400

    Update

--- projeto1/src/index.js
+++ projeto1/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from "./templates/Home";
+import Home from "./templates/Home/index";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
