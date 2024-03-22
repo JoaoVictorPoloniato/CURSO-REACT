10331defa639526fece28b8f15e05c7cd55fe2e8
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-03-22 10:14:31 -0400

    Update de final da aula de jest parte1
    Update de final da aula de jest parte1

--- projeto1/src/components/button/Button.spec.jsx
+++ projeto1/src/components/button/Button.spec.jsx
@@ -1,11 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Button  from '.';
 
 describe('<Button />', () => {
     it('Should render the button with the text "Load More"', () => {
         render(<Button text="Load More"/>);
+        expect.assertions(1);
 
         const button = screen.getByRole('button', { name: /load more/i });
         expect(button).toBeInTheDocument();
+    });
+
+    it('should call function on button click', () => {
+        const fn = jest.fn();
+        render(<Button text="Load More" onClick={fn}/>);
+
+        const button = screen.getByRole('button', { name: /load more/i });
+        
+
+        fireEvent.click(button);
+
+        expect(fn).toHaveBeenCalled();
+    });
+
+    
+    it('should be disabled when disabled is true', () => {
+        const fn = jest.fn();
+        render(<Button text="Load More" disabled={true}/>);
+
+        const button = screen.getByRole('button', { name: /load more/i });
+        
+        expect(button).not.toBeEnabled();
     });
 });
